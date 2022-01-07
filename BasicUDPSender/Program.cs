8d70deb7a48c85a8816b8866533469b27b86e066
Emil Wilhelm Winther <emil377g@edu.easj.dk>
2022-01-07 12:08:41 +0100

    Add project files.

---
+++ BasicUDPSender/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using PlayerLib;
+
+namespace BasicUDPSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Ready to write UDP message");
+                string message = Console.ReadLine();
+
+                UdpClient socket = new UdpClient();
+                SendData(socket, message);
+                Thread.Sleep(4000);
+            }
+        }
+
+        public static void SendData(UdpClient socket, string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            socket.Send(data, data.Length, "255.255.255.255", 6000);
+        }
+    }
+}
